27a4959cf902e0a26fd63b19f991561c48f5411e
rraagg <rob.a.gallagher@gmail.com>
2025-07-03 21:13:35 -0700

    playing with bytes and receiver methods

--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 
 	"github.com/a-h/templ"
 )
 
+type user struct {
+	firstName []byte
+	lastName  []byte
+	email     []byte
+}
+
+func (u user) FirstName() []byte {
+	return u.firstName
+}
+
+func (u user) LastName() []byte {
+	return u.lastName
+}
+
+func (u user) FullName() []byte {
+	sliceOfByteSlices := [][]byte{u.firstName, u.lastName}
+	return bytes.Join(sliceOfByteSlices, []byte(" "))
+}
+
+func (u user) Email() []byte {
+	return u.email
+}
+
 func main() {
-	component := hello("rraagg")
+	webUser := user{
+		firstName: []byte("rraagg"),
+		lastName:  []byte("thegreat"),
+	}
+	component := hello(string(webUser.FullName()))
 	http.Handle("/", templ.Handler(component))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
