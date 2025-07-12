ed9cf2dde4489e007dfdf59fe8b48f239638b2d6
rraagg <rob.a.gallagher@gmail.com>
2025-07-11 21:15:57 -0700

    add db

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/a-h/templ"
 )
@@ -32,11 +36,30 @@ func (u user) Email() []byte {
 }
 
 func main() {
+	log.Println("Starting up the rraagg app!")
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStatement := `
+	CREATE TABLE IF NOT EXISTS users (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	name TEXT
+	);
+	`
+	_, err = db.Exec(sqlStatement)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Table users created successfully")
 	webUser := user{
 		firstName: []byte("rraagg"),
 		lastName:  []byte("thegreat"),
 	}
-	component := hello(string(webUser.FullName()))
+	component := Hello(string(webUser.FullName()))
 	http.Handle("/", templ.Handler(component))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
